2cdfc5c3583637ed31ad69487c53edbe906b96ac
nomver <52451381+Engr-Abdullah-1721@users.noreply.github.com>
2020-11-03 23:10:54 +0500

    atlas

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const url="mongodb://localhost:27017/WORKSHOP1";
 
-mongoose.connect(process.env.MONGODB_URI || url,{useNewUrlParser:true},(err)=>{
+mongoose.connect('mongodb+srv://abdullah:097vehicle@cluster1.toux6.mongodb.net/vehicle-repairing?retryWrites=true&w=majority' || url,{useNewUrlParser:true},(err)=>{
     if(!err){
         console.log("WORKSHOP1 successfully connected");
     }
